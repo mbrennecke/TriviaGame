edf3e26ce73b0fe48708e35abdf69e461d8acf58
Matthew Brennecke <mbrennecke10@gmail.com>
2018-08-06 20:29:24 -0600

    page styled, javascript started

---
+++ assets/javascript/app.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+
+	var questions = ["Who was the legendary Benedictine monk who invented champagne?",
+	"Name the largest freshwater lake in the world?",
+	"Where would you find the Sea of Tranquility?",
+	"What is someone who shoes horses called?",
+	"What item of clothing was named after its Scottish inventor?",
+	"What kind of weapon is a falchion?",
+	"Which word goes before vest, beans and quartet?",
+	"What is another word for lexicon?",
+	"What is the seventh planet from the sun?",
+	"Who invented the rabies vaccination?"];
+
+	var answers = [["Dom Perignon", "Abbot Morris", "Pope Pius", "Don Amici"],
+	["Lake Superior", "Lake Geneva", "Lake Victoria", "Lake Huron"],
+	["The Moon", "Mars", "Texas", "Tatoine"],
+	["A farrier", "A ferrier", "An ironmonger", "A terrier"],
+	["A Mackintosh", "An Apple", "A Cardigan", "A Haggis"],
+	["A sword", "A computer", "A coat", "An umbrella"],
+	["String", "Sweater", "Green", "Mock"],
+	["Dictionary", "Thesaurus", "Tyrannasaurus", "Day planner"],
+	["Uranus", "Pluto", "Jupiter", "Mercury"],
+	["Louis Pasteur", "Louis Armstrong", "Marie Curie", "Albert Einstein"]];
+
+	
+
+});
