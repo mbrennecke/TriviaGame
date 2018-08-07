a12f32282b1303acbd6d2940302b009e9a7ca6f3
MBrennecke <MBrennecke10@gmail.com>
2018-08-07 13:38:51 -0600

    timer working, still need to add stuff?

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,54 +11,93 @@ $(document).ready(function() {
 	"What is the seventh planet from the sun?",
 	"Who invented the rabies vaccination?"];
 
-	var answers = [["Abbot Morris", "Dom Perignon*", "Pope Pius", "Don Amici"],
-	["Lake Geneva", "Lake Victoria", "Lake Huron", "Lake Superior*"],
-	["Mars", "Texas", "The Moon*", "Tatoine"],
-	["A ferrier", "A farrier*", "An ironmonger", "A terrier"],
-	["An Apple", "A Cardigan", "A Mackintosh*", "A Haggis"],
-	["A sword*", "A computer", "A coat", "An umbrella"],
-	["Sweater", "String*", "Green", "Mock"],
-	["Dictionary*", "Thesaurus", "Tyrannasaurus", "Day planner"],
-	["Pluto", "Jupiter", "Mercury", "Uranus*"],
-	["Louis Armstrong", "Marie Curie", "Louis Pasteur*", "Albert Einstein"]];
+	var answers = [["Abbot Morris", "Dom Perignon", "Pope Pius", "Don Amici"],
+	["Lake Geneva", "Lake Victoria", "Lake Huron", "Lake Superior"],
+	["Mars", "Texas", "The Moon", "Tatoine"],
+	["A ferrier", "A farrier", "An ironmonger", "A terrier"],
+	["An Apple", "A Cardigan", "A Mackintosh", "A Haggis"],
+	["A sword", "A computer", "A coat", "An umbrella"],
+	["Sweater", "String", "Green", "Mock"],
+	["Dictionary", "Thesaurus", "Tyrannasaurus", "Day planner"],
+	["Pluto", "Jupiter", "Mercury", "Uranus"],
+	["Louis Armstrong", "Marie Curie", "Louis Pasteur", "Albert Einstein"]];
+	
+	var correctAnswer = ["answer2", "answer4", "answer3", "answer2", "answer3", "answer1", "answer3", "answer1", "answer4", "answer3"];
 
 	var qCounter = 0;
-	var correctAnswer;
+	var countdown = 5;
+	var intervalId;
 	
-	function reset() {
+	
+	function gameReset() {
 		qCounter = 0;
-		correctAnswer = "";
+		questionGen();
 	}
 	
-	function answerChecker(check) {
-		var correct;
-		if (check.indexOf("*") >0){
-			var arr = check.split("");
-			correct = arr.pop();
-			return correct;
-		} else {return;}
+	function run() {
+      clearInterval(intervalId);
+      intervalId = setInterval(counter, 1000);
+    }
+	
+	function counter(){
+		countdown--;
+		$("#timer").text(countdown);
+		if(countdown == 0) {
+			clearInterval(intervalId);
+			$(".question").text("Sorry, you ran out of time. The correct answer is:");
+			setFontColorCorrect();
+			qCounter++;
+			if (qCounter < answers.length){
+				questionCorrectDisplay();
+			}
+		}
+		
+		
 	}
 	
-	function answerFix(answer, index) {
-		var fix = answers[answer][index];
-		fix = fix.slice(0,-1);
-		answers[answer][index] = fix;
-		console.log(answers[answer][index]);
+	function setFontColorCorrect() {
+		var set = "#"+correctAnswer[qCounter];
+		$(set).css("color", "#c70039");
+	}
+	
+	function setFontColorReset() {
+		$(".answer").css("color", "#212529");	
+	}
+	
+	function questionCorrectDisplay() {
+		var delay2 = setTimeout(function() {
+          questionGen();
+		  setFontColorReset();
+        }, 2000);
+		countdown = 5;
 	}
 	
 	function questionGen() {
 		$(".question").text(questions[qCounter]);
 		for (var a=0; a<4; a++) {
-			answerCheck = answerChecker(answers[qCounter][a]);
-			if (answerCheck == "*") {
-				answerFix(qCounter, a);
-				correctAnswer = a+1;
-			}
 			var answer = '#answer' + (a + 1);
 			$(answer).text(answers[qCounter][a]);
+			setFontColorReset();
 		}
+		run();
 	}
+	
+	$(".answer").on("click", function () {
+		var set = "#"+correctAnswer[qCounter];
+		if (event.target.id == correctAnswer[qCounter]){
+			$(".question").text("That is correct!");
+			setFontColorCorrect();
+			qCounter++;
+		} else {
+			$(".question").text("Sorry, the correct answer is:");
+			setFontColorCorrect();
+			qCounter++;
+		}
+		questionCorrectDisplay();
+	});
+	
+	
 
-	questionGen();
+	gameReset();
 	
 });
