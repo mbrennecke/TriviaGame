6642ce2d2b06d96c8fbd42b8a8a1bdc36350b7b7
Matthew Brennecke <mbrennecke10@gmail.com>
2018-08-08 19:13:50 -0600

    Added comments to javascript

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,8 +9,8 @@ $(document).ready(function() {
 	"Which word goes before vest, beans and quartet?",
 	"What is another word for lexicon?",
 	"What is the seventh planet from the sun?",
-	"Who invented the rabies vaccination?"];
-
+	"Who invented the rabies vaccination?"];	
+	
 	var answers = [["Abbot Morris", "Dom Perignon", "Pope Pius", "Don Amici"],
 	["Lake Geneva", "Lake Victoria", "Lake Huron", "Lake Superior"],
 	["Mars", "Texas", "The Moon", "Tatoine"],
@@ -21,6 +21,9 @@ $(document).ready(function() {
 	["Dictionary", "Thesaurus", "Tyrannasaurus", "Day planner"],
 	["Pluto", "Jupiter", "Mercury", "Uranus"],
 	["Louis Armstrong", "Marie Curie", "Louis Pasteur", "Albert Einstein"]];
+
+//Hard coded correct answers, because for the parameters of the assignment it was
+//the best easiest method
 	
 	var correctAnswer = ["answer2", "answer4", "answer3", "answer2", "answer3", "answer1", "answer3", "answer1", "answer4", "answer3"];
 
@@ -29,11 +32,14 @@ $(document).ready(function() {
 	var intervalId;
 	var right = 0;
 	var wrong = 0;
+
+//function for game start, creates the button to begin game
 	
 	function gameStart(){
 		$(".endGame").html('<br><br><button type="button" class="btn btn-primary btn-lg btn-block">Play?</button>');
 		}
 	
+//function to initialize all settings for game start or replay
 	
 	function gameReset() {
 		qCounter = 0;
@@ -41,17 +47,23 @@ $(document).ready(function() {
 		wrong = 0;
 		questionGen();
 	}
+
+//Starts the clock countdown for questions
 	
 	function run() {
       clearInterval(intervalId);
       intervalId = setInterval(counter, 1000);
     }
 	
+//manual counter to progress game	
+	
 	function counter(){
 		countdown--;
+		//countdown clock
 		$("#timer").text(countdown);
 		if(countdown == 0) {
 			clearInterval(intervalId);
+			//if clock reaches zero, lose condition for question
 			$(".question").text("Sorry, you ran out of time. The correct answer is:");
 			setFontColorCorrect();
 			qCounter++;
@@ -64,18 +76,27 @@ $(document).ready(function() {
 		}	
 	}
 	
+//Function to set the answers area after button press
+	
 	function setAnswersList() {
 		$(".endGame").html('<ul><li id="answer1" class="answer "></li><li id="answer2" class="answer "></li><li id="answer3" class="answer "></li><li id="answer4" class="answer "></li></ul>')
 	}
+
+//Function to set color of correct answer
 	
 	function setFontColorCorrect() {
 		var set = "#"+correctAnswer[qCounter];
 		$(set).css("color", "#c70039");
 	}
 	
+//Function to reset color of answer divs after display of correct answer
+//Last correct answer div was remaining colored without this 
+	
 	function setFontColorReset() {
 		$(".answer").css("color", "#212529");
 	}
+
+//Function to display the correct answer for 2 seconds after guess or timer event
 	
 	function questionCorrectDisplay() {
 		var delay2 = setTimeout(function() {
@@ -85,6 +106,8 @@ $(document).ready(function() {
         }, 2000);
 		countdown = 31;
 	}
+
+//Function to create the visible question
 	
 	function questionGen() {
 		if (qCounter < questions.length){
@@ -98,15 +121,22 @@ $(document).ready(function() {
 		
 		run();
 		$(".answer").css("pointer-events", "auto");
+	//End of questions reached, replay button presented with right and wrong guesses 
+	//shown
 		} else {$(".question").text("");
 				$(".endGame").html('<h3>Game Over</h3><h3>Correct:'+right+'<//h3><h3>Wrong:'+wrong+'</h3><button type="button" class="btn btn-primary btn-lg btn-block">Play Again?</button>');}
 	}
+
+//Event listner for answer selection
 	
 	$(document).on("click", ".answer", function () {
 		var set = "#"+correctAnswer[qCounter];
+		//correct answer chosen
 		if (event.target.id == correctAnswer[qCounter]){
 			clearInterval(intervalId);
 			$(".question").text("That is correct!");
+			//it was possible to click spam and progress the questions from this view
+			//this turns of clicks for the time the correct answer shown
 			$(".answer").css("pointer-events", "none");
 			setFontColorCorrect();
 			qCounter++;
@@ -114,13 +144,17 @@ $(document).ready(function() {
 		} else {
 			clearInterval(intervalId);
 			$(".question").text("Sorry, the correct answer is:");
+			//Same again, to stop click spam of answers
 			$(".answer").css("pointer-events", "none");
 			setFontColorCorrect();
 			qCounter++;
 			wrong++;
 		}
+		//Call to function to show correct answer in list
 		questionCorrectDisplay();
 	});
+
+//Listener event for start game or restart game
 	
 	$(document).on('click', '.btn', function () {
 		setAnswersList();
@@ -128,7 +162,7 @@ $(document).ready(function() {
 		gameReset();
 	});
 
-		
+//Starts the game on page load
 	gameStart();
 	
 });
